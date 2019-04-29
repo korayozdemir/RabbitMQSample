8fda50222e6031d489355021bf178cff1862f4d2
Koray Özdemir <koray_ozdemir@msn.com>
2019-04-29 17:23:14 +0300

    First Commit

---
+++ RabbitMQ.Receiving/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Receiving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //Bu kısımda okuyacağımız Queue yi belirtiyoruz.
+                    channel.QueueDeclare(queue: "MyFirstQueue" ,
+                            durable: false ,
+                            exclusive: false ,
+                            autoDelete: false ,
+                            arguments: null);
+
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    //Received sayesinde sürekli bir listening yapılması sağlanıyor
+                    consumer.Received += (model , ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}" , message);
+                    };
+                    //Mesajları çekme işlemleri
+                    //autoAck : true olmsaı 
+                    channel.BasicConsume(queue: "MyFirstQueue" ,
+                        autoAck: true ,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RabbitMQ ya bağlantı.Sunucu üzerinde ise Ip bilgisi yer alacak.
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+
+            using (var connection = factory.CreateConnection())
+            {
+                //Mesajı iletmek için kanal oluşutuyoruz.
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("MyFirstQueue" ,
+                                false , //Fiziksel mi saklanacak memory üzerinde mi ?
+                                false , //Farklı bağlantılar ile kullanım
+                                false , //Consumer lar kullandıktan sonra otomatik silinmesi
+                                arguments: null); //Exchange tipleri
+
+                    string message = JsonConvert.SerializeObject("Hello World");
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "" ,
+                        routingKey: "MyFirstQueue" ,
+                        basicProperties: null ,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}" , message);
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
